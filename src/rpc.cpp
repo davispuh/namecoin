c81e16768a8e0a3e8f20863ab5ace0afe1375b4f
Vincent Durham <vinced@vinced.bit>
2011-09-18 03:52:12 -0400

    Create new block in parent chain when aux merkle changes

--- src/rpc.cpp
+++ src/rpc.cpp
@@ -1484,6 +1484,7 @@ Value getwork(const Array& params, bool fHelp)
         static int64 nStart;
         static CBlock* pblock;
         if (pindexPrev != pindexBest ||
+            vchAux != vchAuxPrev ||
             (nTransactionsUpdated != nTransactionsUpdatedLast && GetTime() - nStart > 60))
         {
             if (pindexPrev != pindexBest)
@@ -1496,6 +1497,7 @@ Value getwork(const Array& params, bool fHelp)
             }
             nTransactionsUpdatedLast = nTransactionsUpdated;
             pindexPrev = pindexBest;
+            vchAuxPrev = vchAux;
             nStart = GetTime();
 
             // Create new block
@@ -1595,6 +1597,7 @@ Value getworkaux(const Array& params, bool fHelp)
 
     if (params.size() == 1)
     {
+        static vector<unsigned char> vchAuxPrev;
         vector<unsigned char> vchAux = ParseHex(params[0].get_str());
 
         // Update block
